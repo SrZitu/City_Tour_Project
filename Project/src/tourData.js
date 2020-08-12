27f1885bb7e66c701f0f1a1d03da1c3f348a3629
SrZitu <zitu094@gmail.com>
2020-08-12 08:32:39 +0600

    tour commit

---
+++ Project/src/tourData.js
@@ -0,0 +1,51 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+
+  {
+    id: 3,
+    city: "London",
+    img: "./img/london.jpg",
+    name: "London sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 4,
+    city: "Dubai",
+    img: "./img/dubai.jpeg",
+    name: "Dubai sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 5,
+    city: "Maldives",
+    img: "./img/mal.jpeg",
+    name: "Maldives sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 6,
+    city: "Switzerland",
+    img: "./img/swi.jpeg",
+    name: "Switzerland  tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+];
